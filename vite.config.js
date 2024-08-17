c57df825e1882d58204a72f01b87a568f3b6d395
R1lty <tiv51tiv51@gmail.com>
2024-08-18 00:16:06 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Укажите имя репозитория
+  base: '/R1lty.github.io', // Укажите имя репозитория
 });
