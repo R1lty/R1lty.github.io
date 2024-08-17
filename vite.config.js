4bdcbdef4a144fb2381678af259dc44a36f69252
R1lty <tiv51tiv51@gmail.com>
2024-08-18 00:12:09 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/egorfy', // Укажите имя репозитория
+  base: '/', // Укажите имя репозитория
 });
